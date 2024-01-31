76e49e2bc9ddcc5ab231e1ecfe5ed6177af1e9d8
Srinivas Andukuri <92281933+andukurisanthosh@users.noreply.github.com>
2024-01-31 12:01:35 +0530

    modified contact form and included functionality to send message

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,8 +2,24 @@ import React from 'react'
 import './ContactFormStyles.css'
 import { FaLinkedin, FaGithub } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function ContactForm() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_yl2hxqh', 'template_xu32ihy', form.current, 'bn0eHys0yPLq_XdIi')
+            .then((result) => {
+                console.log(result.text);
+                alert("Message sent successfully")
+            }, (error) => {
+                console.log(error.text);
+                alert("Unable send the message")
+            });
+    };
     return (
         <div className='form'>
             <div className='connections'>
@@ -21,11 +37,11 @@ function ContactForm() {
                 </Link>
 
             </div>
-            <form>
-                <input type='text' placeholder='Name'></input>
-                <input type='text' placeholder='Email'></input>
-                <input type='text' placeholder='Subject'></input>
-                <textarea rows={'6'} placeholder='Type your message here ...'></textarea>
+            <form ref={form} onSubmit={sendEmail}>
+                <input type='text' placeholder='Name' name='from_name'></input>
+                <input type='text' placeholder='Email' name='from_email'></input>
+                <input type='text' placeholder='Subject' name='from_subject'></input>
+                <textarea rows={'6'} placeholder='Type your message here ...' name='message'></textarea>
                 <button className='btn'>Submit</button>
             </form>
 
